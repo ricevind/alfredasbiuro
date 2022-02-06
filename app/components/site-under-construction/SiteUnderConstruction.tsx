c2128722c9a7504b7d5a8b94bbc2db897ff4ffc9
michal.kwasniewski <michal.kwasniewski@dotdata.com>
2022-02-06 21:13:47 +0100

    prettier setup

--- app/components/site-under-construction/SiteUnderConstruction.tsx
+++ app/components/site-under-construction/SiteUnderConstruction.tsx
@@ -1,19 +1,22 @@
 export const SiteUnderConstruction = ({
   rightImage,
 }: {
-  rightImage: { url: string; alt: string };
+  rightImage: {
+    url: string;
+    alt: string;
+  };
 }) => {
   return (
-    <div className="bg-zinc-100 ">
-      <div className="min-h-screen w-screen  py-4 relative flex flex-col max-w-screen-2xl mx-auto">
-        <div className="px-4 mr-auto md:z-10 md:my-auto">
-          <h1 className="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl">
+    <div className="bg-zinc-100">
+      <div className="relative mx-auto flex  min-h-screen  w-screen max-w-screen-2xl flex-col py-4">
+        <div className="mr-auto px-4 md:z-10 md:my-auto">
+          <h1 className="font text-4xl font-extrabold tracking-tight text-gray-900 sm:text-6xl">
             Alfreda Sobierajska
           </h1>
-          <h2 className="text-2xl font font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="font text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             KANCELARIA <br /> PODATKOWO-UBEZPIECZENIOWA
           </h2>
-          <h2 className="text-2xl font font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="font text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             BIURO RACHUNKOWE
           </h2>
 
@@ -23,20 +26,20 @@ export const SiteUnderConstruction = ({
 
           <a
             href="http://alfredasbiuro.wix.com/alfredasobierajska"
-            className="mt-4 inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-indigo-700"
+            className="mt-4 inline-block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-center font-medium text-white hover:bg-indigo-700"
           >
             Aktualna strona
           </a>
         </div>
+      </div>
 
-        <div className="mt-4 xl:mt-0 xl:absolute xl:right-0  top-0 bottom-0 flex flex-col justify-center">
-          <div aria-hidden="true" className="pointer-events-none">
-            <img
-              className="h-full w-full"
-              src={rightImage.url}
-              alt={rightImage.alt}
-            />
-          </div>
+      <div className="top-0 bottom-0 mt-4 flex  flex-col justify-center xl:absolute xl:right-0 xl:mt-0">
+        <div aria-hidden="true" className="pointer-events-none">
+          <img
+            className="h-full w-full"
+            src={rightImage.url}
+            alt={rightImage.alt}
+          />
         </div>
       </div>
     </div>
